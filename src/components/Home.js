1a9582ca9ae4ee3fb09d846188fb4567dceafe24
tunsay <tuna1997@hotmail.fr>
2023-02-21 17:08:54 +0100

    Change name App to Home

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+      </header>
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './components/App';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
