389ffdd5b1fc987acfe8ab47e93ba4872fe9c46c
tunsay <tuna1997@hotmail.fr>
2023-02-21 17:32:59 +0100

    Create Home route

---
+++ src/pages/App.js
@@ -0,0 +1,14 @@
+import Home from './Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
