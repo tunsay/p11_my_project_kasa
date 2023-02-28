237c0596909ac623eaeb10d231a4fc431e25db0b
tunsay <tuna1997@hotmail.fr>
2023-02-28 20:04:39 +0100

    add route path to page in App

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,11 +1,26 @@
+import './App.scss'
+import Layout from '../../layouts/Layout/Layout'
 import Home from '../Home/Home'
+import About from '../About/About'
+import Logement from '../Logement/Logement'
+import Error from '../Error/Error'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const logements = require('../../datas/logements.json')
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route
+            path="logement/:id"
+            element={<Logement logements={logements} />}
+          />
+          <Route path="*" element={<Error />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
